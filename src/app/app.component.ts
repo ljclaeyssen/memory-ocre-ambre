d4f6278ea4cf5fa1ab994e246a7ddbb22d11af64
ljclaeyssen <ljclaeyssen@gmail.com>
2025-06-14 14:59:57 +0200

    fix: route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import {Component} from '@angular/core';
-import {RouterLink, RouterOutlet} from "@angular/router";
 import {Button} from "primeng/button";
 import {Dialog} from "primeng/dialog";
-import {Toolbar} from "primeng/toolbar";
 import {InfosComponent} from "./infos/infos.component";
 import {OcreMemoryComponent} from "./ocre-memory/ocre-memory.component";
 
 @Component({
     selector: 'app-root',
-  imports: [OcreMemoryComponent, Toolbar, Button, RouterOutlet, RouterLink, Dialog, InfosComponent],
+  imports: [OcreMemoryComponent, Button, Dialog, InfosComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,3 @@
 import {Routes} from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'play',
-    loadComponent: () => import('./ocre-memory/ocre-memory.component').then(m => m.OcreMemoryComponent)
-  },
-  {
-    path: 'info',
-    loadComponent: () => import('./infos/infos.component').then(m => m.InfosComponent)
-  },
-  {
-    path: '**',
-    redirectTo: 'play',
-  }
-];
+export const routes: Routes = [];
